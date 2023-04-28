9807483c9d03dc62cb4a5db8ec393f54e2177df8
yun <jan2218@naver.com>
2023-04-28 20:43:08 +0900

    react 4월 28일

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -7,27 +7,39 @@ function TodoList() {
 
   useEffect(() => {
     const data = [
+      {id:1, text:"산책 가기", completed:false}, { id:2, text:"멋사 과제", completed:true},
+      { id:3, text:"야구 보기", completed:false}
       // 이곳에 초기 렌더링 시 표시 될 '객체 형태의 할 일들'을 작성해주세요. (hint: 객체의 key는 id, text, completed 입니다.)
     ];
+    setTodos(data);
     // 생성한 todo를 state의 상태 변수인 todos 에 저장해주세요
   }, []);
 
   useEffect(() => {
+    var n = 0;
+    for(var i=0; i<todos.length; i++) {
+      if(todos[i].completed === true){
+        n++;
+      }      
+    }
+    setCount(n);
     // 이곳에 todos에 변화가 생길 때마다 완료한 할 일의 개수(count)를 'update' 하도록 하는 코드를 작성해주세요. (+ dependency array 에는 어떤 값이 들어가야 할까요?)
-  }, []);
+  }, [todos]);
 
   useEffect(() => {
+     if(count===todos.length && count>0) alert("오늘 할 일을 모두 완료했습니다");
     // 이곳에 count의 update를 감지하면서 모든 할 일 모두 완료했을 때 "오늘 할 일을 모두 완료하셨네요!"를 출력하는 알림창이 뜨도록 코드를 작성해주세요. (+ dependency array 에는 어떤 값이 들어가야 할까요?)
-  }, []);
+  }, [count]);
 
   const handleInput = (e) => {
+     setInputValue(e.target.value);
     // 이곳에 입력창에 입력한 값이 state의 상태 변수인 inputValue에 저장되도록 코드를 작성해주세요.
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const newTodo = {id: Date.now() /* 이곳에 새로운 todo의 text와 completed 속성을 설정하도록 코드를 작성해주세요 */};
-    setTodos(/* 이곳에 기존의 todos에 새로 생성된 newTodo를 추가하도록 하는 코드를 작성해주세요 (hint: 배열에서의 spread 연산자 활용) */);
+    const newTodo = {id: Date.now(), text:inputValue, completed:false /* 이곳에 새로운 todo의 text와 completed 속성을 설정하도록 코드를 작성해주세요 */};
+    setTodos([...todos, newTodo]/* 이곳에 기존의 todos에 새로 생성된 newTodo를 추가하도록 하는 코드를 작성해주세요 (hint: 배열에서의 spread 연산자 활용) */);
     setInputValue("");
   };
 
@@ -35,6 +47,7 @@ function TodoList() {
     const updatedTodos = todos.map((todo) => {
       if (todo.id === id) {
         return {
+          ...todo, completed:!todo.completed
           /* 이곳에 기존의 todo의 객체 속성에서 completed 객체의 속성만 변경되도록 하는 코드를 작성해주세요 (hint: 객체에서의 spread 연산자 활용) */
         };
       }
